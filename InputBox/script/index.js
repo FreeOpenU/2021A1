6ef3388e721f909f04521cb86a5db1758c88af15
Feiyi-Ding <dingfeiyi10@gmail.com>
2021-05-25 20:27:09 -0400

    update submit requirement

--- InputBox/script/index.js
+++ InputBox/script/index.js
@@ -1,3 +1,14 @@
-﻿function nlp() {
+﻿var clicked = false
+let btn = document.querySelector('#nlp');
+btn.addEventListener("click", function () {
+    clicked = true;
     document.querySelector('#result').value = "NLP result";
-}
+})
+
+
+let btnS = document.querySelector('#submit');
+btnS.addEventListener("click", function () {
+    if (clicked == false){
+        alert("Please check the NLP result first.")
+    }
+})
