b678308488bdd8fb5007ead7625f2e8e5cff6e83
Feiyi-Ding <dingfeiyi10@gmail.com>
2021-05-26 17:59:27 -0400

    database connection

--- InputBox/script/index.js
+++ InputBox/script/index.js
@@ -12,3 +12,21 @@ btnS.addEventListener("click", function () {
         alert("Please check the NLP result first.")
     }
 })
+
+/*var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()*/
