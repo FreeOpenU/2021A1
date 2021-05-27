3e6b0c3c070b51d5920036ec874f32fdbce35848
Feiyi-Ding <62368623+Feiyi-Ding@users.noreply.github.com>
2021-05-27 16:01:33 -0400

    Update index.js

--- InputBox/script/index.js
+++ InputBox/script/index.js
@@ -1,6 +1,6 @@
 ﻿var clicked = false
-let btn = document.querySelector('#nlp');
-btn.addEventListener("click", function () {
+let btn_nlp = document.querySelector('#nlp');
+btn_nlp.addEventListener("click", function () {
     clicked = true;
     document.querySelector('#result').value = "NLP result";
 })
@@ -12,21 +12,3 @@ btnS.addEventListener("click", function () {
         alert("Please check the NLP result first.")
     }
 })
-
-/*var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'dbuser',
-  password: 's3kreee7',
-  database: 'my_db'
-})
-
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
-})
-
-connection.end()*/
