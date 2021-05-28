549d61bf0e451443d2e03fd8a479263009fd3365
Debankita15 <43865554+Debankita15@users.noreply.github.com>
2021-05-28 14:54:23 -0400

    Add files via upload

--- connection.php
+++ connection.php
@@ -4,14 +4,14 @@
    $dbpass = 'xzsawq21xzsawq21';
    $dbname = 'D2021A1'
    
-   $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+   $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
    
    if (mysqli_connect_errno()) {
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
    exit();
    }
    
-   mysqli_query($conn, "SELECT * FROM Form");
+   mysqli_query($con, "SELECT * FROM Form");
    echo "Affected rows: " . mysqli_affected_rows($con);
 
 
