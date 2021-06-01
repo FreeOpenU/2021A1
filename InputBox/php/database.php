40c255f3243e792ba0b10fbccaf1802e15ce90d7
Feiyi-Ding <62368623+Feiyi-Ding@users.noreply.github.com>
2021-06-01 18:29:02 -0400

    enable submit button after check nlp

--- InputBox/php/database.php
+++ InputBox/php/database.php
@@ -1,22 +1,27 @@
 <?php
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('localhost', 'root', '','inputbox');
+if(isset($_POST['statement'])){
+	// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+	$con = mysqli_connect('localhost', 'root', '','inputbox');
 
-// get the post records
+		// get the post records
 
-$firstN = $_POST['firstN'];
-$lastN = $_POST['lastN'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$statement = $_POST['statement'];
+	$firstN = $_POST['firstN'];
+	$lastN = $_POST['lastN'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$statement = $_POST['statement'];
 
-// database insert SQL code
-$sql = "INSERT INTO `input` (`firstName`, `lastName`, `email`, `phone`, `statement`) VALUES ('$firstN', '$lastN', '$email', '$phone', '$statement')";
+	// database insert SQL code
+	$sql = "INSERT INTO `input` (`firstName`, `lastName`, `email`, `phone`, `statement`) VALUES ('$firstN', '$lastN', '$email', '$phone', '$statement')";
 
-// insert in database 
-$rs = mysqli_query($con, $sql);
-if($rs)
-{
-	echo "Statement received!";
+	// insert in database 
+	$rs = mysqli_query($con, $sql);
+	if($rs)
+	{
+		echo "Statement received!";
+	}
+}
+else{
+	echo "Please check the NLP result first.";
 }
 ?>
--- InputBox/script/index.js
+++ InputBox/script/index.js
@@ -1,14 +1,9 @@
 ﻿var clicked = false
+var btnSubmit = document.getElementById("submit");
+btnSubmit.disabled = true;
 let btn_nlp = document.querySelector('#nlp');
 btn_nlp.addEventListener("click", function () {
     clicked = true;
     document.querySelector('#result').value = "NLP result";
-})
-
-
-let btnS = document.querySelector('#submit');
-btnS.addEventListener("click", function () {
-    if (clicked == false){
-        alert("Please check the NLP result first.")
-    }
+    btnSubmit.disabled = false;
 })
