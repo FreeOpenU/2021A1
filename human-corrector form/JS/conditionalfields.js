244f3041758a3381893ee35edde06b012a2f0e8c
Debankita15 <debankitabasu@gmail.com>
2021-06-14 10:57:30 -0400

    Added separate folders for each file to make it look clean

---
+++ human-corrector form/JS/conditionalfields.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    $('#choice').change(function () {
+        var selected = $('#choice option:selected').text();
+        if(selected=="All"){
+            $('#sentence_relationship, #word_insight').show();
+        }
+        else{
+        $('#sentence_relationship').toggle(selected == "Sentences");
+        $('#word_insight').toggle(selected == "Words");
+        $('#paragraph_analyze').toggle(selected == "Paragraph");
+        }
+    });
+    
+    });
