da68a78ee442bf96ae0991f5045c939eedf74789
Debankita15 <debankitabasu@gmail.com>
2021-07-27 11:44:40 -0400

    new php script added

--- human-corrector form/JS/buttontoaddtextfield.js
+++ human-corrector form/JS/buttontoaddtextfield.js
@@ -1,28 +1,44 @@
 $(document).ready(function(){
-  $("#add1").on("click", function(){
-    var new_chq_no = parseInt($('#total_chq').val()) + 1;
-    var new_input = "<input  id='statement_" + new_chq_no + "field' name='statement_field' type='text' class='form-control' value='default value'>";
-    $("#new_chq").append(new_input);
+    
+  var x = 1; //Initial field counter is 1
+  
+  //Once add button is clicked
+  $('#add1').click(function(){
+    var fieldHTML = '<div><input id="statement_field" name="statement_field[]" type="text" class="form-control" value="default value"/>'; //New input field html 
+          x++; //Increment field counter
+          $('#new_statement').append(fieldHTML); //Add field html
+      
   });
+
+   var x = 1; //Initial field counter is 1
   
-  $("#add2").on("click", function(){
-    var new_word_no = parseInt($('#total_wrd').val()) + 1;
-    var word_input = "<input  id='word_" + new_word_no + "field' name='word_" + new_word_no +"field' type='text' class='form-control' value='default value'>";
-    $("#new_wrd").append(word_input);
+  //Once add button is clicked
+  $('#add2').click(function(){
+    var fieldHTML = '<div><input id="wordsyn_field" name="wordsyn_field[]" type="text" class="form-control" value="default value"/>'; //New input field html 
+          x++; //Increment field counter
+          $('#new_wordsyn').append(fieldHTML); //Add field html
+      
+   });
+
+   var x = 1; //Initial field counter is 1
+  
+   //Once add button is clicked
+   $('#add3').click(function(){
+    var fieldHTML = '<div><input id="diction_field" name="diction_field[]" type="text" class="form-control" value="default value"/>'; //New input field html 
+          x++; //Increment field counter
+          $('#new_diction').append(fieldHTML); //Add field html
+      
+   });
+
+   
+  var x = 1; //Initial field counter is 1
+  
+  //Once add button is clicked
+  $('#add4').click(function(){
+    var fieldHTML = '<div><input id="tone_field" name="tone_field[]" type="text" class="form-control" value="default value"/>'; //New input field html
+          x++; //Increment field counter
+          $('#new_tone').append(fieldHTML); //Add field html
+      
   });
-  
-  $("#add3").on("click", function(){
-      var new_tone_no = parseInt($('#total_tone').val()) + 1;
-      var tone_input = "<input  id='tone_" + new_tone_no + "field' name='tone_" + new_tone_no +"field' type='text' class='form-control' value='default value'>";
-      $("#new_tone").append(tone_input);
-    });
-  
-    $("#add4").on("click", function(){
-      var new_diction_no = parseInt($('#total_diction').val()) + 1;
-      var diction_input = "<input  id='tone_" + new_diction_no + "field' name='tone_" + new_diction_no +"field' type='text' class='form-control' value='default value'>";
-      $("#new_diction").append(diction_input);
-    });
-  
-  });
-  
-  
+});
+
