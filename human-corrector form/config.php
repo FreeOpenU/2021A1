69477c69e320130db1c8be7fa23f8e73de0610c4
Sean <yh712@scarletmail.rutgers.edu>
2021-09-23 10:44:31 -0400

    Modify config.php

--- human-corrector form/config.php
+++ human-corrector form/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "testDB";
+$username = "d2021a";
+$password = "xzsawq21xzsawq21";
+$dbname = "D2021A1";
 
 // Connect to the MySQL server
 $conn = new mysqli($servername, $username, $password, $dbname) or die("Connection failed: " . $conn->connect_error);
